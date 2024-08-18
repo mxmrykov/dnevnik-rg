2a2fa268b6e133bb0bbf1e07e62a7bd491caa484
Maxim Rykov <rykov.maksim2@wb.ru>
2024-08-18 15:44:48 +0300

    разделение деплоя

--- src/domain/constants/api.ts
+++ src/domain/constants/api.ts
@@ -1,6 +1,12 @@
 import axios from "axios";
 
-export const SERVER: string = "http://localhost:8000";
+let api_port: string = "8000"
+
+console.log(process.env)
+
+if (process.env.BUILD_ENV === "stage") api_port = "8001"
+
+export var SERVER: string = "http://localhost:" + api_port;
 
 export const API_GROUP_V1_ROUTE: string = "/api/v1";
 
