d2b8e16d9c59365526fc80cf48997dab8e72d7ac
Maxim Rykov <rykov.maksim2@wb.ru>
2024-08-26 00:26:27 +0300

    деплой

--- src/domain/constants/api.ts
+++ src/domain/constants/api.ts
@@ -2,9 +2,7 @@ import axios from "axios";
 
 let api_port: string = "8000"
 
-console.log(process.env)
-
-if (process.env.BUILD_ENV === "stage") api_port = "8001"
+if (process.env.REACT_APP_BUILD_E === "stage") api_port = "8001"
 
 export var SERVER: string = "http://localhost:" + api_port;
 
